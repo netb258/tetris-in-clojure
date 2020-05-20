61f52bf4a59bf0881c68cdff6d7dfcd0e0dc0416
netb258 <netb258@abv.bg>
2020-05-20 19:08:59 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]] ;; Graphics library for Clojure.
+                 [quil "2.7.1"]] ;; Graphics library for Clojure.
   :main tetris.core
   :aot :all)
